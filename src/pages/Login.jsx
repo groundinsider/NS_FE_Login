9c6c5f9abc1296e7928c4fb8c6ced450d7b11471
groundinsider <groundinsider24@gmail.com>
2024-09-02 13:32:43 +0900

    v2

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -36,76 +36,30 @@ function Login() {
       <div className="header">Login</div>
       <div className="login-box">
         <div className="left">
-          <div>
-            <span>Student ID</span>
-            <input
-              className="set_data"
-              value={input.studentId}
-              onChange={onChangeInput}
-              name="studentId"
-            />
-          </div>
-          <div>
-            <span>Password</span>
-            <input
-              className="set_data"
-              value={input.password}
-              onChange={onChangeInput}
-              name="password"
-              type="password"
-            />
-          </div>
-          <input type = "submit" className="login-button" value = "Login" />
+          
         </div>
 
         <div className="right">
-          <div>
-            <span>Student ID</span>
+        <div>
             <input
               className="set_data"
-              value={user.studentId}
-              onChange={onChangeUser}
+              value={input.studentId}
+              placeholder="아이디"
+              onChange={onChangeInput}
               name="studentId"
             />
           </div>
           <div>
-            <span>Your Name</span>
             <input
               className="set_data"
-              value={user.name}
-              onChange={onChangeUser}
-              name="name"
-            />
-          </div>
-          <div>
-            <span>Password</span>
-            <input
-              className="set_data"
-              value={user.password}
-              onChange={onChangeUser}
+              value={input.password}
+              placeholder="비밀번호"
+              onChange={onChangeInput}
               name="password"
               type="password"
             />
           </div>
-          <div>
-            <span>Phone Number</span>
-            <input
-              className="set_data"
-              value={user.phoneNumber}
-              onChange={onChangeUser}
-              name="phoneNumber"
-            />
-          </div>
-          <div>
-            <span>Your email</span>
-            <input
-              className="set_data"
-              value = {user.email}
-              onChange = {onChangeUser}
-              name = "email"
-            />
-          </div>
-          <input type = "submit" className="login-button" value = "Create"/>
+          <input type = "submit" className="login-button" value = "로그인" />
         </div>
       </div>
       <div className="footer"></div>
